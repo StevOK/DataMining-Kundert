cf38c6eb3d657e457b5ce5474796eda112ec6d79
Steven Kundert <stevenkundert@yahoo.com>
2017-09-29 09:45:59 -0500

    Update process_lot.py

--- Assignments/process_single_lot/process_lot.py
+++ Assignments/process_single_lot/process_lot.py
@@ -1,5 +1,6 @@
 # Steven Kundert, Jiaxing Liu, Prakriti Pandey
 # CMPS 5443 - Data Mining - Griffin
-# Assignment 5 - Occupied Parking Space Project, part 1
+# Assignment 5 - Occupied Parking Space Project
+# Part 1 - Image Processing
 # 10/2/17
 
