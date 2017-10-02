faf463e02bdf02ac2c293643fdb065dcbd145c0b
Steven Kundert <stevenkundert@yahoo.com>
2017-10-01 20:47:11 -0500

    Update process_lot.py

--- Assignments/process_single_lot/process_lot.py
+++ Assignments/process_single_lot/process_lot.py
@@ -4,3 +4,34 @@
 # Part 1 - Image Processing
 # 10/2/17
 
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+import json
+
+
+img = cv.imread('parking1b.jpg')
+
+#plt.imshow(img)
+#plt.show()
+
+#img = cv.rectangle(img, (974,1036-974), (491,599-491), (0,255,0), 4)
+    
+#font = cv.FONT_HERSHEY_SIMPLEX
+#cv.putText(img, "This is a rectangle", (230, 50), font, 0.8, (0, 255, 0), 2, cv.LINE_AA)
+           
+
+crop_img = img[451:586,701:849]
+    
+
+plt.imshow(img)
+plt.show()
+plt.imshow(crop_img)
+plt.show()
+
+hsv = cv.cvtColor(crop_img,cv.COLOR_BGR2HSV)
+hist = cv.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
+np.savetxt('test.out', hist, delimiter=',')
+
+plt.imshow(hist,interpolation = 'nearest')
+plt.show()
